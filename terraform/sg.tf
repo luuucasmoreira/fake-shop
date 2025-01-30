ecdccb595951f31fbf0e04c196554252019f58c7
Lucas <luuucasmoreira1994@gmail.com>
2025-01-30 16:36:51 -0300

    CI: Ajuste no terraforms

---
+++ terraform/sg.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "eks_security_group" {
+    name        = "eks_security_group"
+    description = "Security group for EKS cluster"
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow HTTPS traffic"
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow HTTP traffic"
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow all outbound traffic"
+    }
+
+    tags = {
+        Name = "eks_security_group"
+    }
+}
